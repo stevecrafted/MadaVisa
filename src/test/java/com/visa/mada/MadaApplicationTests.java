3af4b81a953ac822c67c7cdd6223c010e31d0e6e
steve-daniel0 <stevens.rakotomalala@gmail.com>
2026-04-15 18:33:12 +0300

    Initial commit

---
+++ src/test/java/com/visa/mada/MadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visa.mada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
